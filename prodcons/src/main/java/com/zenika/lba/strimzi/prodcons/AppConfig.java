2a9c1c86e9ded6e0625bffad58cf5b5122de871c
Laurent Baudet <laurent.baudet.partner@decathlon.com>
2023-01-02 13:25:33 +0100

    initial

---
+++ prodcons/src/main/java/com/zenika/lba/strimzi/prodcons/AppConfig.java
@@ -0,0 +1,12 @@
+package com.zenika.lba.strimzi.prodcons;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class AppConfig {
+
+    
+    
+}
